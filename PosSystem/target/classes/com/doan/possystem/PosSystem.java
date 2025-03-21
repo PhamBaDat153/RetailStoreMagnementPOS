cb3eead3d6ee4f6235952e7d00abd71facc542be
Phạm Bá Đạt <pham.ba.dat153@gmail.com>
2025-03-21 20:11:04 +0700

    Add files via upload
    customer panel done

---
+++ PosSystem/target/classes/com/doan/possystem/PosSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.doan.possystem;
+
+/**
+ *
+ * @author phamb
+ */
+public class PosSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
